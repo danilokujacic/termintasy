6878fb43e0e0835b81b566c0d39925fefcb1b5e6
danilokujacic <danilo.kujacic01@gmail.com>
2025-01-10 10:42:46 +0100

    change to prod api

--- apps/termintasy/src/app/environments/environment.ts
+++ apps/termintasy/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://nestjs-app:3000',
+  apiUrl: 'https://api.terminta.xyz/api',
   clientSecret: 'GOCSPX-WciK6y2dup03_PHjqolfinymcdW',
   clientId:
     '266707580114-4ma5s9lki3rf302i8ao67161allrvbri.apps.googleusercontent.com',
