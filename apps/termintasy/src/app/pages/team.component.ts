7f59775d576c00328f48a4664a092934a500189e
DKAtlas <58274332+danilokujacic@users.noreply.github.com>
2025-01-10 20:43:59 +0100

    add transfers

--- apps/termintasy/src/app/pages/team.component.ts
+++ apps/termintasy/src/app/pages/team.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { map, switchMap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { LayoutComponent } from '../layout/layout.component';
+import { UserService } from '../user/user.service';
+import { PlayersWrapperComponent } from '../create-team/players-wrapper.component';
+import { PlayersService } from '../create-team/players.service';
 
 @Component({
   selector: 'app-team',
-  imports: [CommonModule, LayoutComponent],
+  imports: [CommonModule, LayoutComponent, PlayersWrapperComponent],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css',
 })
@@ -21,11 +24,38 @@ export class TeamComponent implements OnInit {
     atk1: null,
     atk2: null,
   });
+  playersDrawer = signal<boolean | string>(false);
+  teamId = signal<string>('');
+  teamPlayers = signal<any[]>([]);
+  transferComponent = signal(false);
+  currentTeamPlayer = signal<number | null>(null);
+  playersToTransfer = signal<[number, number][]>([]);
+  teamPlayersToSend = signal<number[]>([]);
+  selectedPlayers = signal<string[]>([]);
+  transfers = signal<number | null>(null);
+  isOwner = signal(false);
+  teamTransfers = computed(
+    () =>
+      (this.transfers() ?? 0) - this.playersToTransferTeamPlayersDif().length
+  );
+  playersToTransferTeamPlayersDif = computed(() =>
+    this.playersToTransfer().filter(([_, transferPlayer]) => {
+      return (
+        this.teamPlayers().findIndex((el) => el.id === transferPlayer) === -1
+      );
+    })
+  );
   teamName = signal('');
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private userService: UserService,
+    private router: Router,
+    private playersService: PlayersService
+  ) {}
 
   playerPoints(player: any) {
-    const gameStats = player.gameStats[0];
+    const gameStats = player?.gameStats?.[0];
 
     return gameStats?.points || 0;
   }
@@ -46,6 +76,15 @@ export class TeamComponent implements OnInit {
           atk1: null,
           atk2: null,
         };
+
+        this.userService.me().subscribe((user: any) => {
+          this.isOwner.set(user.id === data.ownerId);
+        });
+
+        this.teamId.set(data.id);
+
+        this.transfers.set(data.transfers);
+        this.teamPlayers.set(data.players);
         data.players.forEach((player: any) => {
           if (player.position === 'GK') {
             players.gk = player;
@@ -69,5 +108,70 @@ export class TeamComponent implements OnInit {
         this.players.set(players);
         this.teamName.set(data.name);
       });
+  }
+
+  queryPlayers(
+    position: 'GK' | 'ATK' | 'MID' | 'DEF',
+    mapPosition: 'gk' | 'def1' | 'def2' | 'mid' | 'atk1' | 'atk2',
+    player: any
+  ) {
+    this.playersService.getPlayers(position).subscribe((data) => {
+      this.playersService.playerState.set({ loading: false, data });
+      this.playersDrawer.set(mapPosition);
+    });
+
+    this.playersToTransfer.update((prevState) =>
+      prevState.filter(([_, transferPlayer]) => transferPlayer !== player.id)
+    );
+
+    const teamPlayers = Object.values(this.players());
+    const playersDiff = this.playersToTransferTeamPlayersDif();
+    const playersThatAreTransfered = this.teamPlayers().filter(
+      (player) =>
+        playersDiff.findIndex(([teamPlayer]) => teamPlayer === player.id) !== -1
+    );
+
+    this.selectedPlayers.set([
+      ...playersThatAreTransfered.map((player) => player.name),
+      ...teamPlayers.map((player: any) => player.name),
+      player.name,
+    ]);
+
+    this.currentTeamPlayer.set(player.id);
+  }
+
+  makeTransfer() {
+    this.http
+      .post(
+        environment.apiUrl +
+          '/user-team/' +
+          this.teamId() +
+          '/transfer-players',
+        {
+          players: this.playersToTransferTeamPlayersDif().map(
+            ([teamPlayer]) => teamPlayer
+          ),
+          playersToReceive: this.playersToTransferTeamPlayersDif().map(
+            ([_, transferPlayer]) => transferPlayer
+          ),
+        }
+      )
+      .subscribe(() => {
+        window.location.reload();
+      });
+  }
+  onSelect({ player, position }: any) {
+    const currentPlayer = this.currentTeamPlayer()!;
+    this.playersDrawer.set(false);
+
+    this.playersToTransfer.update((prevState) => [
+      ...prevState,
+      [currentPlayer, player.id],
+    ]);
+
+    this.players.update((prevState) => ({
+      ...prevState,
+      [position]: { ...player },
+    }));
   }
 }
