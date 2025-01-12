cb97628e3fdd9aa956112ca1ef963ddf312ab124
DKAtlas <58274332+danilokujacic@users.noreply.github.com>
2025-01-12 14:29:24 +0100

    save token

--- apps/termintasy/src/app/push-notification/push-notification.service.ts
+++ apps/termintasy/src/app/push-notification/push-notification.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {
   MessagePayload,
@@ -6,6 +7,7 @@ import {
   onMessage,
 } from '@angular/fire/messaging';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +16,7 @@ export class MessagingService {
   private messageSource = new BehaviorSubject<MessagePayload | null>(null);
   currentMessage = this.messageSource.asObservable();
 
-  constructor(private messaging: Messaging) {}
+  constructor(private messaging: Messaging, private http: HttpClient) {}
 
   requestPermission() {
     return getToken(this.messaging, {
@@ -22,7 +24,13 @@ export class MessagingService {
         'BOFMA8d-khvtMqYCm6PvnC5lRHSf6YRiH77h39caEK0tpNqx7LeaFmghmU0vR9h-X10p4O6sff4KNLm6gc5k8m8',
     })
       .then((token) => {
-        console.log('Token:', token);
+        this.http
+          .post(environment.apiUrl + '/push-notification/save-token', {
+            token,
+          })
+          .subscribe();
+
+        console.log(token);
       })
       .catch((err) => {
         console.error('Permission denied', err);
