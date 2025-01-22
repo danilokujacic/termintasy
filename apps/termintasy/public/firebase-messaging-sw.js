8cc662bb52c793c842eb4bc8aaa40d2fe31218de
danilokujacic <danilo.kujacic01@gmail.com>
2025-01-22 11:43:19 +0100

    add proper service worker config

--- apps/termintasy/public/firebase-messaging-sw.js
+++ apps/termintasy/public/firebase-messaging-sw.js
@@ -1,24 +1,24 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
 
 firebase.initializeApp({
+  apiKey: 'AIzaSyAsCn-JTYjJubpQ53EyzlbMIgbFWLIpde0',
+  authDomain: 'termintasy.firebaseapp.com',
+  projectId: 'termintasy',
+  storageBucket: 'termintasy.firebasestorage.app',
   messagingSenderId: '824710831706',
+  appId: '1:824710831706:web:1955cb588a8ec30fe0c263',
+  measurementId: 'G-WRYS215176',
 });
 
 const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  );
-  const notificationTitle = 'Background Message from html';
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png',
+    body: payload.notification.body,
   };
 
-  return self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
