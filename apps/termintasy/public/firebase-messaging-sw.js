df5e5795938d52430cc52e5ce2aa5b2559995def
danilokujacic <danilo.kujacic01@gmail.com>
2025-01-22 11:53:08 +0100

    modify sw

--- apps/termintasy/public/firebase-messaging-sw.js
+++ apps/termintasy/public/firebase-messaging-sw.js
@@ -1,7 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
-
-firebase.initializeApp({
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
+const firebaseConfig = {
   apiKey: 'AIzaSyAsCn-JTYjJubpQ53EyzlbMIgbFWLIpde0',
   authDomain: 'termintasy.firebaseapp.com',
   projectId: 'termintasy',
@@ -9,23 +8,23 @@ firebase.initializeApp({
   messagingSenderId: '824710831706',
   appId: '1:824710831706:web:1955cb588a8ec30fe0c263',
   measurementId: 'G-WRYS215176',
-});
-
+}; // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler(function (payload) {
+messaging.onBackgroundMessage((payload) => {
   console.log(
     '[firebase-messaging-sw.js] Received background message ',
     payload
   );
-  const notificationTitle = 'Background Message from html';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png',
+    body: payload.notification.body,
   };
-
   return self.registration.showNotification(
     notificationTitle,
     notificationOptions
   );
+});
+self.addEventListener('notificationclick', (event) => {
+  console.log(event);
 });
