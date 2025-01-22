c8dcfc8ad2e3b7ef290490ebb26620f7414f8724
danilokujacic <danilo.kujacic01@gmail.com>
2025-01-22 11:45:32 +0100

    add proper firebase versions

--- apps/termintasy/public/firebase-messaging-sw.js
+++ apps/termintasy/public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
 firebase.initializeApp({
   apiKey: 'AIzaSyAsCn-JTYjJubpQ53EyzlbMIgbFWLIpde0',
